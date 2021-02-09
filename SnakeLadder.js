30fbc37fde86c23f39ed76e5973e39c1d8b53761
Aanchalt14 <aanchalt71@gmail.com>
2021-02-10 01:18:52 +0530

    Add : Repeat till player win the match

--- SnakeLadder.js
+++ SnakeLadder.js
@@ -1,8 +1,45 @@
 console.log("Welcome to Snake and Ladder Game ");
 
 class SnakeAndLadder {
+    constructor() { }
     main() {
         var player = 1;
-        const initialpos = 1;
+        const initialpos = 0;
+        this.currentPos = initialpos;
+        var winningPosition = 100;
+        for(let i=currentPos;i<winningPosition;i++)
+            var dieMove = this.rollDie();
+            this.currentPosition = this.checkOption(this.currentPos, dieMove);
+            console.log("PlayerPosition = " + this.currentPos);
+        }
     }
+    rollDie() {
+        return Math.floor(Math.random() * 6) + 1;
+    }
+
+    checkOption(currentPos, dieMove) {
+        var NO_PLAY = 0;
+        var LADDER = 1;
+        var SNAKE = 2;
+        this.option = Math.floor(Math.random() * 3);
+
+        switch (this.option) {
+            case NO_PLAY:
+                currentPos += 0;
+                break;
+            case LADDER:
+                currentPos = currentPos + dieMove;
+                break;
+            case SNAKE:
+                if ((currentPos - dieMove) <= 0)
+                    currentPos = 0;
+                else
+                    currentPos = currentPos - dieMove;
+                break;
+        }
+        return currentPos;
+    }
+
 }
+var obj1 = new SnakeAndLadder();
+obj1.main();
