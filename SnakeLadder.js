06ac70b90e5ee1b782d19d8033c103cc199ccd16
Aanchalt14 <aanchalt71@gmail.com>
2021-02-10 00:30:06 +0530

    Add : Rolling die and get one face

--- SnakeLadder.js
+++ SnakeLadder.js
@@ -1,8 +1,17 @@
 console.log("Welcome to Snake and Ladder Game ");
 
 class SnakeAndLadder {
+    constructor() { }
     main() {
         var player = 1;
         const initialpos = 1;
     }
+    
+        rolldie() 
+        {
+         console.log(Math.floor(Math.random() * 6) + 1);
+        }
 }
+
+var object = new SnakeAndLadder();
+object.rolldie();
